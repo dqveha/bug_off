7078b6cb14833c32e12ac0e11338be900eb7e756
Dave Lindqvist <Lindqvist.Dave@gmail.com>
2021-09-27 13:52:25 -0700

    Seed data with Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+account = User.create!(:email => "dave@gmail.com", :password => "password", :admin => true, :role => "user")
+
+10.times do |user_instance|
+  User.create!(:email => Faker::Internet.email, :password => "password", :role => "user")
+  User.create!(:email => Faker::Internet.email, :password => "password", :role => "support")
+end
+
+p "Created #{User.count} users"
