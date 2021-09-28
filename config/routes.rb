293f0120999502e62f1bb75b9f0a93fd7c81c821
Dave Lindqvist <Lindqvist.Dave@gmail.com>
2021-09-28 13:28:15 -0700

    Add user pages for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  resources :users
   resources :bug_tickets
+
   root to: "welcome#home"
   
   get 'welcome/home'
-  get 'admin/index'
-  
+
   devise_for :users
 end
