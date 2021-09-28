5ae42ce73456911b32a250e12787bbc6aabf8f04
Dave Lindqvist <Lindqvist.Dave@gmail.com>
2021-09-28 13:51:21 -0700

    Revert "Add user pages for admin"
    This reverts commit 293f0120999502e62f1bb75b9f0a93fd7c81c821.

--- app/models/bug_ticket.rb
+++ app/models/bug_ticket.rb
@@ -1,10 +1,4 @@
 class BugTicket < ApplicationRecord
   has_many :bug_ticket_users
   has_many :users, :through => :bug_ticket_users 
-
-  validates :bug_behavior, presence: true, length: { in: 50..1250 }
-  validates :environment, presence: true, length: { in: 50..1250 }
-  validates :status, presence: true
-  validates :priority, presence: true
-  validates :owner, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
   resources :bug_tickets
-
   root to: "welcome#home"
   
   get 'welcome/home'
-
+  get 'admin/index'
+  
   devise_for :users
 end
